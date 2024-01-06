e6294bcbac807aedf238fa2e54b5afe8d90cbb58
Sun Serega <sunserega2@gmail.com>
2024-01-06 09:56:06 +0200

    Use `HandleOrbitalPhysicsUnderThrustStart`

--- SASed Warp dll/Plugin.cs
+++ SASed Warp dll/Plugin.cs
@@ -1,10 +1,9 @@
 ﻿
 using System;
 using System.Reflection;
+using System.Collections.Concurrent;
 
 using BepInEx;
-
-using UnityEngine;
 
 using SpaceWarp;
 using SpaceWarp.API.Mods;
@@ -12,9 +11,11 @@ using SpaceWarp.API.Assets;
 using SpaceWarp.API.UI.Appbar;
 
 using KSP.Sim;
+using KSP.Sim.impl;
 using KSP.Game;
 using KSP.Messages;
-using KSP.Sim.impl;
+
+using HarmonyLib;
 
 namespace SASedWarp
 {
@@ -77,12 +78,16 @@ namespace SASedWarp
 			Appbar.RegisterAppButton(
 				"SASed Warp",
 				"BTN-SASed-Warp",
-				AssetManager.GetAsset<Texture2D>($"{SpaceWarpMetadata.ModID}/images/icon.png"),
+				AssetManager.GetAsset<UnityEngine.Texture2D>($"{SpaceWarpMetadata.ModID}/images/icon.png"),
 				open =>
 				{
 					menu_open = open;
 				}
 			);
+
+			VesselComponent_HandleOrbitalPhysicsUnderThrustStart_Patch.log = Logger;
+			// For now - skip this patch, it ended up being unnecessary
+			//Harmony.CreateAndPatchAll(typeof(SASedWarpPlugin).Assembly);
 
 			base.OnInitialized();
 		}
@@ -96,12 +101,17 @@ namespace SASedWarp
 
 		#region Update
 
+		private static readonly BindingFlags AllBF = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static;
+
 		public void Update()
 		{
 			if (!in_valid_game_state) return;
 			if (!menu_open) return;
+			if (!GameManager.Instance.Game.ViewController.TimeWarp.IsWarping) return;
+			if (GameManager.Instance.Game.ViewController.TimeWarp.IsPhysicsTimeWarp) return;
 
 			var vessel = GameManager.Instance.Game.ViewController.GetActiveSimVessel(true);
+			if (vessel.flightCtrlState.mainThrottle==0) return;
 			// "left"??? it somehow spin-stabilizes direction during warp, lol
 			// "up" only works for ~90 degrees of orbit
 			// And "forward", the most expected thing, rotates 90 degrees up from targ_forward. But unlike "up" it doesn't glich out after a while
@@ -109,7 +119,7 @@ namespace SASedWarp
 			var curr_forward = vessel.transform.left;
 
 			var autopilot = vessel.Autopilot;
-			var telemetry = (TelemetryComponent)autopilot.GetType().GetField("_telemetry", BindingFlags.Instance|BindingFlags.NonPublic).GetValue(autopilot);
+			var telemetry = (TelemetryComponent)autopilot.GetType().GetField("_telemetry", AllBF).GetValue(autopilot);
 			Vector targ_forward;
 			switch (autopilot.AutopilotMode)
 			{
@@ -148,7 +158,7 @@ namespace SASedWarp
 				default:
 					throw new NotImplementedException(autopilot.AutopilotMode.ToString());
 			}
-
+			
 			//Logger.LogDebug($"Vector diff sqr mag={(curr_forward - targ_forward).sqrMagnitude}");
 			//if ((curr_forward - targ_forward).sqrMagnitude==0) return; //TODO <0.01
 
@@ -157,15 +167,49 @@ namespace SASedWarp
 			// And .LookRotation just doesn't do that
 			//var rotation = Rotation.FromTo(curr_forward, targ_forward);
 			var rotation = Rotation.LookRotation(curr_forward, targ_forward);
-
+			
 			//Logger.LogDebug($"Rotating from forward={curr_forward.vector}");
 			//Logger.LogDebug($"Towards prograde={curr_forward.coordinateSystem.ToLocalVector(targ_forward)}");
 			//Logger.LogDebug($"Using rotation={rotation.localRotation}");
 			vessel.transform.UpdateRotation(rotation);
 
+			// Recalculate the trajectory using new vessel rotation
+			//var sw = System.Diagnostics.Stopwatch.StartNew();
+			vessel.GetType().GetMethod("HandleOrbitalPhysicsUnderThrustStart", AllBF).Invoke(vessel, null);
+			//Logger.LogDebug($"Recalculated thrust-warp trajectory in {sw.Elapsed}");
+
+			if (vessel.IsOrbitalPhysicsUnderThrustActive) return;
+			// If not, there was an error in KSP code (look in Alt+C),
+			// at least cancel thrust so it doesn't burn propelant
+			var vehicle = (VesselVehicle)GameManager.Instance.Game.ViewController.GetActiveVehicle(true);
+			vehicle.AtomicSet(mainThrottle: 0, null, null, null, null, null, null, null, null, null, null, null, null, null, null);
+
+			//TODO White box instead of proper message?
+			GameManager.Instance.Game.Notifications.ProcessNotification(new NotificationData
+			{
+				Tier = NotificationTier.Alert,
+				Primary = new NotificationLineItemData { LocKey = "SASedWarp/MathBug" }
+			});
+
 		}
 
 		#endregion
+
+	}
+
+	[HarmonyPatch(typeof(VesselComponent), "HandleOrbitalPhysicsUnderThrustStart", MethodType.Normal)]
+	public class VesselComponent_HandleOrbitalPhysicsUnderThrustStart_Patch
+	{
+		private static readonly ConcurrentDictionary<VesselComponent, byte> all_instances = new ConcurrentDictionary<VesselComponent, byte>();
+		public static BepInEx.Logging.ManualLogSource log = null!; // set in SASedWarpPlugin.OnInitialized
+
+		public static void Prefix(
+			VesselComponent __instance
+		)
+		{
+			var added = all_instances.TryAdd(__instance, 0);
+			log.LogDebug($"HandleOrbitalPhysicsUnderThrustStart: new_inst={added}, total_inst={all_instances.Count}");
+		}
 
 	}
 
